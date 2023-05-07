709f9d3b17a9f8b55f79a01a923945fc70e12563
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2023-05-07 20:04:23 +0200

    Cleaned some code

--- FridgeSide/src/main.rs
+++ FridgeSide/src/main.rs
@@ -2,7 +2,7 @@
 
 use std::path::PathBuf;
 
-use notify::{Watcher, RecursiveMode, watcher};
+use notify::{Watcher, RecursiveMode, watcher, DebouncedEvent};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
@@ -15,20 +15,20 @@ async fn main()
     
     /////////////FILE WATCHER///////////////////////////
     let (sender, receiver) = channel();
-
-    // Create a watcher object, delivering debounced events.
-    // The notification back-end is selected based on the platform.
+    // Create a watcher object, delivering debounced events. The notification back-end is selected based on the platform.
     let mut watcher = watcher(sender, Duration::from_secs(10)).unwrap();
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
+    // Add a path to be watched. All files and directories at that path and below will be monitored for changes.
     watcher.watch(dataPath, RecursiveMode::Recursive).unwrap();
 
     loop {
         match receiver.recv() {
            Ok(event) => {
             println!("modif: {:?}", event);
-            process().await;
+            if let DebouncedEvent::Write(p) = event{
+                println!("Modified file{:?}",p);
+                process(p).await;
+            }
+            
            },
            Err(e) => println!("watch error: {:?}", e),
         }
@@ -36,10 +36,11 @@ async fn main()
 }
 
 use error_chain::error_chain;
-use serde::Deserialize;
+//use serde::Deserialize;
 use serde_json::json;
 use std::env;
 use reqwest::Client;
+use std::fs;
 
 error_chain! {
     foreign_links {
@@ -48,34 +49,30 @@ error_chain! {
     }
 }
 
-//#[derive(Deserialize, Debug)]
-struct Gist {
-    id: String,
-    html_url: String,
-}
-
-async fn process()->Result<()>{
-    let gist_body = json!({
-        "nm": "arthurooo",
-        "description": "on va mettre nos data ici je pense",
-        "public": true,
+async fn process(filePath:PathBuf)->Result<()>{
+    let contents = fs::read_to_string(&filePath)
+        .expect("Should have been able to read the file");
+        
+    let data = json!({
+        "fridgeName": "arthurooo",
+        "path": filePath.into_os_string().into_string(),
+        "contents": contents,
         "files": {
-             "main.rs": {
-             "content": r#"fn main() { println!("hello world!");}"#
+             "file1": {
+             "content": r#"trucs"#
             }
         }});
 
     let request_url = "http://localhost:5000/sendData";
     println!("about to send server data");
-    let response = Client::new()
+    let _response = Client::new()
         .post(request_url)
-        //.basic_auth(gh_user.clone(), Some(gh_pass.clone()))
-        .json(&gist_body)
+        //.basic_auth(gh_user.clone(), Some(gh_pass.clone())) //todo: handle auth
+        .json(&data)
         .send().await?;
     println!("sent !!");
 
-    //let gist: Gist = response.json().await?;
-    //println!("Created {:?}", gist);
+    //let result = response.json().await?;
     
     Ok(())
 }
--- ServerSide/FlaskServer.py
+++ ServerSide/FlaskServer.py
@@ -11,9 +11,16 @@ def sendData():
     print(request.form)
     print(request.json)
 
-    user = request.json["nm"]
+    user = request.json["fridgeName"]
+    
+    f = open("BDD/data", "a")
+    f.write(request.json["contents"])
+    f.close()
     return redirect(url_for('success',name = user))
 
-
+@app.route("/getData/<source>")
+def getData(source):
+    return open("BDD/"+source).read()
+    
 if __name__ == '__main__':
     app.run(debug = True)

1a6ff8cf3378ef36b2c7228872fc8d8e47f6e6f9
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2023-05-11 23:51:35 +0200

    Cleaned some code

--- ServerSide/FlaskServer.py
+++ ServerSide/FlaskServer.py
@@ -1,77 +1,58 @@
-from flask import Flask, redirect, url_for, request, make_response, send_file
+from flask import Flask, redirect, url_for, request, send_file
 app = Flask(__name__)
 
-import influxdb_client, os, time
-import io
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+import influxdb_client, os
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = os.environ.get("INFLUXDB_TOKEN") #export INFLUXDB_TOKEN=QOcPWIZWqhpCwiOMTPxLvK4X-xOIqRzhK3wbBnhYHFG1nTnjcLHzpCPiI2hfwI2S92p8oqzn_LZzefNoZIqLJw==
+#token = os.environ.get("INFLUXDB_TOKEN") #export INFLUXDB_TOKEN=QOcPWIZWqhpCwiOMTPxLvK4X-xOIqRzhK3wbBnhYHFG1nTnjcLHzpCPiI2hfwI2S92p8oqzn_LZzefNoZIqLJw==
 token="QOcPWIZWqhpCwiOMTPxLvK4X-xOIqRzhK3wbBnhYHFG1nTnjcLHzpCPiI2hfwI2S92p8oqzn_LZzefNoZIqLJw=="
 org = "FrigoQ"
 url = "http://localhost:8086"
+bucket="Frigo1"
 
 db_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-
-bucket="Frigo1"
 
 write_api = db_client.write_api(write_options=SYNCHRONOUS)
 query_api = db_client.query_api()
 
-from datetime import datetime
-import pytz
+
 def processLine(line):
     d,t,v=line.split(",")
     if d[0] == " ": d=d[1:]
     measurement = "ens"
     tags="fridgeName=frigo1"
     fields="temp="+v
-    #print(fields)
     
+    from datetime import datetime
+    import pytz
     date= datetime.strptime(d+" "+t,"%d-%m-%y %H:%M:%S")
     local = pytz.timezone("Europe/Paris")
     local_dt = local.localize(date, is_dst=None)
     utc_dt = local_dt.astimezone(pytz.utc)
-    #print(date.tzinfo)
-    #print(date.isoformat())
-    timestamp = str(int(datetime.timestamp(date)))
-    #return measurement+","+tags+" "+fields+" "+timestamp
+
     p = influxdb_client.Point(measurement).field("temp1", float(v)).time(utc_dt.isoformat())
     write_api.write(bucket=bucket, org=org, record=p)
     
-    
-@app.route('/success/<name>')
-def success(name):
-    print("youhou",name)
-    return 'welcome %s' % name
 
 @app.route('/sendData',methods = ['POST'])
 def sendData():
-    #print(request.form)
-    #print(request.json)
+    for line in request.json["contents"].splitlines():
+        processLine(line)
+    
+    return redirect(url_for('getData',source = "d"))
 
-    user = request.json["fridgeName"]
-    #for line in request.json["contents"].splitlines():
-        #print(processLine(line))
-    influxLines = list(map(processLine,request.json["contents"].splitlines()))
-    #print(influxLines)
-    #write_api.write("Frigo1",  influxLines)
-    """f = open("BDD/data", "a")
-    f.write(request.json["contents"])
-    f.close()"""
-    return redirect(url_for('success',name = user))
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 @app.route("/getData/<source>")
-def getData(source):
+def getData(source): #the source parameter is now ignored
     query = """from(bucket: "Frigo1")
                 |> range(start: today())
                 |> filter(fn: (r) => r["_measurement"] == "ens")
                 |> filter(fn: (r) => r["_field"] == "temp1")"""
 
     df = query_api.query_data_frame(org=org, query=query)
-    print(df)
+
+    import plotly.graph_objects as go
+    from plotly.subplots import make_subplots
     fig = make_subplots()
     fig.add_trace(
         go.Scatter(
@@ -81,19 +62,16 @@ def getData(source):
             name="Température en K",
             line=dict(color="blue", width=2),
         ))
-    #fig.show()
-    fig.write_html("test.html")
+    fig.write_html("generated.html")
 
     return open("template.html",encoding="utf-8").read()
-    #return open("BDD/"+source).read()
     
-@app.route("/getGenerated/<file>")
+@app.route("/getGenerated/<file>") #Used to return the generated html (embedded in the template.html) (this might evolve later)
 def generated(file):
     return open(file,encoding="utf-8").read()
 
-@app.route("/images/<file>")
+@app.route("/images/<file>") #Used to send the favicon
 def images(file):
-    print("dfvfdvdvdv", file)
     return send_file(file,mimetype='image/png')
 
 if __name__ == '__main__':
--- ServerSide/viz.py
+++
@@ -1,4 +0,0 @@
-import plotly.express as px
-
-fig =px.scatter(x=range(10), y=range(10))
-fig.write_html("test.html")

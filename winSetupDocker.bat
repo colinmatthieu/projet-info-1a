63c26b6ca59952ae74edc1fa0ec292c27ac3012d
nicolas moreau <nicolas.moreau@etu.minesparis.psl.eu>
2023-06-26 19:47:28 +0200

    Added grafana docker launch in winSetupDocker.bat Fixed a bug regarding spaces in paths

--- winSetupDocker.bat
+++ winSetupDocker.bat
@@ -1,4 +1,5 @@
-docker run -d -p 8086:8086 --name FrigoDB -v %cd%/ServerSide/influxDB_volume/data:/var/lib/influxdb2 -v %cd%/ServerSide/influxDB_volume/config:/etc/influxdb2 -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=admin -e DOCKER_INFLUXDB_INIT_PASSWORD=adminadmin -e DOCKER_INFLUXDB_INIT_ORG=FrigoQ -e DOCKER_INFLUXDB_INIT_BUCKET=Frigo1 influxdb:latest
+docker run -d -p 8086:8086 --name FrigoDB -v "%cd%/ServerSide/influxDB_volume/data":"/var/lib/influxdb2" -v "%cd%/ServerSide/influxDB_volume/config":"/etc/influxdb2" -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=admin -e DOCKER_INFLUXDB_INIT_PASSWORD=adminadmin -e DOCKER_INFLUXDB_INIT_ORG=FrigoQ -e DOCKER_INFLUXDB_INIT_BUCKET=Frigo1 influxdb:latest
+docker run -d --name grafana -p 3000:3000 -v "%cd%/ServerSide/grafana_volume":"/var/lib/grafana" grafana/grafana-oss:10.0.0
 
 echo off
 :waitloop

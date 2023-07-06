dc512316820a43301da1fdf74e98ef85502e5440
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2023-07-06 11:51:17 +0200

    Fix wrong signature on plugins

--- winSetupDocker.bat
+++ winSetupDocker.bat
@@ -1,5 +1,5 @@
 docker run -d -p 8086:8086 --name FrigoDB -v "%cd%/ServerSide/influxDB_volume/data":"/var/lib/influxdb2" -v "%cd%/ServerSide/influxDB_volume/config":"/etc/influxdb2" -e DOCKER_INFLUXDB_INIT_MODE=setup -e DOCKER_INFLUXDB_INIT_USERNAME=admin -e DOCKER_INFLUXDB_INIT_PASSWORD=adminadmin -e DOCKER_INFLUXDB_INIT_ORG=FrigoQ -e DOCKER_INFLUXDB_INIT_BUCKET=Frigo1 influxdb:latest
-docker run -d --name grafana -p 3000:3000 -v "%cd%/ServerSide/grafana_volume":"/var/lib/grafana" grafana/grafana-oss:10.0.0
+docker run -d --name grafana -p 3000:3000 -v "%cd%/ServerSide/grafana_volume":"/var/lib/grafana" -e "GF_INSTALL_PLUGINS=nline-plotlyjs-panel, grafana-oncall-app" grafana/grafana-oss:10.0.0
 
 echo off
 :waitloop
@@ -8,5 +8,8 @@ echo "Loading... be patient ..."
 sleep 5
 goto waitloop
 :waitloopend
+docker network create FridgeWatcher
+docker network connect FridgeWatcher grafana
+docker network connect FridgeWatcher FrigoDB
 echo on
 launch.bat

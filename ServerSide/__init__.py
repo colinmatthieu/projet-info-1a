d43673b186c3ab26372147d62e75b058451cec40
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2023-07-07 01:32:34 +0200

    Added deadman alerts !!

--- ServerSide/__init__.py
+++ ServerSide/__init__.py
@@ -22,6 +22,32 @@ buckets_api = db_client.buckets_api()
 
 db = SQLAlchemy()
 
+from apscheduler.schedulers.background import BackgroundScheduler
+def deadmanAlert(sender,fridge,measure):
+    #sender="ensParis"
+    #fridge="CH1"
+    #measure="T"
+    print("Deadman check:", sender,fridge,measure)
+    
+    tables = query_api.query('from(bucket:"'+sender+'") |> range(start: 0, stop: now()) |> filter(fn: (r) => r["_measurement"] == "'+fridge+'")|> filter(fn: (r) => r["_field"] == "'+measure+'")|> last()')
+    if len(tables) == 0:
+        return "NEWDATA"
+    import pytz
+    import datetime
+    d = tables[0].records[0]["_time"].astimezone(pytz.timezone("Europe/Paris"))#.strftime("%d-%m-%y,%H:%M:%S")
+    print("Last recorded value:",d)
+    now = datetime.datetime.now().astimezone(pytz.timezone("Europe/Paris"))
+    print("Current time:",now)
+    minutesSinceData = (now - d).total_seconds()/60
+    print(minutesSinceData)
+    if minutesSinceData > 2:
+        import requests
+        webhook = "https://hooks.slack.com/services/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"
+        message = {"text": f"Deadman alert !\nHaven't received news from {sender} - {fridge} in {str(minutesSinceData)} minutes !"}
+        x = requests.post(webhook, json = message)
+        print("Deadman triggered !", x)
+    return d #please
+    
 def create_app():
     app = Flask(__name__)
 
@@ -53,5 +79,9 @@ def create_app():
 
     from .grafana import grafana as grafana_blueprint
     app.register_blueprint(grafana_blueprint)
+    
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(deadmanAlert, 'interval',["ensParis", "demo_T", "T0"], seconds=30)
+    scheduler.start()
 
     return app

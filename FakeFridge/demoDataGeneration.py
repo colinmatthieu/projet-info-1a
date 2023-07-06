f1c4c6722633b721165d3fb9a4120e06b500442b
nicolas moreau <nicolas.moreau@etu.minesparis.psl.eu>
2023-07-06 23:45:59 +0200

    working demo

--- FakeFridge/demoDataGeneration.py
+++ FakeFridge/demoDataGeneration.py
@@ -2,29 +2,54 @@
 from random import randint,uniform
 from time import sleep
 from datetime import datetime
+import numpy as np
 
 
 DATA_PATH = "../TestData"
 
-def genDataLine():
+def genDataLine_T(value,i):
     contents = []#["01-01-21"]#,"11:50:35"] #format: "01-01-21,11:50:35,2.880740E+2"
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     current_date = now.strftime("%d-%m-%y")
     contents.append(current_date)
     contents.append(current_time)
-    contents.append(str(uniform(10.54e-9,+10.57e-9)))
+    value = 0.01 + np.sin(value/10)*0.005 + 0.005 * i
+    value += 0.01 * value/50
+    value += np.random.normal(0,0.0005,1)[0]
+    
+    contents.append(str(value))
     return ",".join(contents)+"\n"
+
+def genDataLine_P(value,i):
+    contents = []#["01-01-21"]#,"11:50:35"] #format: "01-01-21,11:50:35,2.880740E+2"
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_date = now.strftime("%d-%m-%y")
+    contents.append(current_date)
+    contents.append(current_time)
+    value = 10.54e-9 + np.sin(value/10)*10.54e-9/2
+    value += 10.54e-9 * value/50
+    value += np.random.normal(0,10.54e-9 / 20,1)[0]
+    
+    contents.append(str(value))
+    return ",".join(contents)+"\n"
+
 def launchFridge():
+    t = 0
     while True:
-        sleep(randint(1,5)) # Sleep a random number of seconds (between 1 and 5)
-        data=""
-        data+=genDataLine()
-        print("Generated lines:")
-        print(data)
-        f=open(DATA_PATH+"/21-01-01/generated.txt","a")
-        f.write(data)
-        f.close()
+        t+=1.5
+        sleep(4) # Sleep a random number of seconds (between 1 and 5)
+        for i in range(4):
+            T_data=genDataLine_T(t/(i+1) + i*542,i)
+            P_data=genDataLine_P(t/(i+1) + i*542,i)
+            print("Generated lines:" + T_data + " and " + P_data)
+            f_T=open(DATA_PATH+"/21-01-01/demo_T T" + str(i) + " date.txt","a")
+            f_T.write(T_data)
+            f_T.close()
+            f_P=open(DATA_PATH+"/21-01-01/demo_P P" + str(i) + " date.txt","a")
+            f_P.write(P_data)
+            f_P.close()
         
 if __name__ == "__main__":
     print("Starting fake fridge")

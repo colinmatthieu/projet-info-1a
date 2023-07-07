7585650beaad48a7d9eb9a75e956c67b91ad7872
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2023-07-07 10:05:47 +0200

    Tauri tick + removed unused tauri code + fixed zoom on demo dashboards

--- tauri/fridgewatcher/src-tauri/src/main.rs
+++ tauri/fridgewatcher/src-tauri/src/main.rs
@@ -17,86 +17,6 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-async fn start_logger() 
-{
-    println!("In logger");
-    let mut dataPath = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    dataPath.push("..");
-    dataPath.push("..");
-    dataPath.push("..");
-    dataPath.push("TestData");
-    println!("Data path: {}", dataPath.display());
-    
-    /////////////FILE WATCHER///////////////////////////
-    let (sender, receiver) = channel();
-    // Create a watcher object, delivering debounced events. The notification back-end is selected based on the platform.
-    let mut watcher = watcher(sender, Duration::from_secs(3)).unwrap();
-    // Add a path to be watched. All files and directories at that path and below will be monitored for changes.
-    watcher.watch(dataPath, RecursiveMode::Recursive).unwrap();
-
-    loop {
-        match receiver.recv() {
-           Ok(event) => {
-            println!("modif: {:?}", event);
-            if let DebouncedEvent::Write(p) = event{
-                println!("Modified file{:?}",p);
-                thread::spawn(|| {process(p);});
-            }
-            
-           },
-           Err(e) => println!("watch error: {:?}", e),
-        }
-    }
-}
-
-use error_chain::error_chain;
-//use serde::Deserialize;
-use serde_json::json;
-use std::env;
-use reqwest::blocking::Client;
-use std::fs;
-
-error_chain! {
-    foreign_links {
-        EnvVar(env::VarError);
-        HttpRequest(reqwest::Error);
-    }
-}
-use std::fs::metadata;
-fn process(filePath:PathBuf)->Result<()>{
-    let md = metadata(&filePath).unwrap();
-    if md.is_dir() {
-        println!("Ignoring directory: {:?}", filePath);
-        return Ok(())
-    }
-    let contents = fs::read_to_string(&filePath)
-        .expect("Should have been able to read the file");
-        
-    let data = json!({
-        "fridgeName": "arthurooo",
-        "path": filePath.into_os_string().into_string(),
-        "contents": contents,
-        "files": {
-             "file1": {
-             "content": r#"trucs"#
-            }
-        }});
-
-    let request_url = "http://localhost:5000/sendData";
-    println!("about to send server data");
-    let _response = Client::new()
-        .post(request_url)
-        //.basic_auth(gh_user.clone(), Some(gh_pass.clone())) //todo: handle auth
-        .json(&data)
-        .send();//.await?;
-    println!("sent !!");
-
-    //let result = response.json().await?;
-    
-    Ok(())
-}
-
 fn main() {
   let tray_menu = SystemTrayMenu::new(); // insert the menu items here
   tauri::Builder::default()
@@ -137,7 +57,7 @@ fn main() {
       }
       _ => {}
     })
-    .invoke_handler(tauri::generate_handler![greet, start_logger])
+    .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- tauri/fridgewatcher/src/main.js
+++ tauri/fridgewatcher/src/main.js
@@ -1,8 +1,7 @@
 const { invoke } = window.__TAURI__.tauri;
 const Command = window.__TAURI__.shell.Command
 
-const command = Command.sidecar('logger/logger')
-const output = await command.execute()
+const command = Command.sidecar('../../../FridgeSide/target/x86_64-pc-windows-msvc/release/FridgeSide')
 
 let greetInputEl;
 let greetMsgEl;
@@ -13,12 +12,6 @@ async function greet() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
   document.querySelector("#start-logger-button").addEventListener("click", (e) => {
     invoke('start_logger').then(() => {console.log("Promise received");
     document.querySelector("#start-logger-button").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#8000ff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
@@ -26,7 +19,7 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("Logger started");
   });
   document.querySelector('#start-logger-window-button').addEventListener("click", (e) => {
-    console.log("Logger window loop")
+    document.querySelector('#start-logger-window-button').innerHTML = 'Logger started ! <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#8000ff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>'
     command.execute().then(() => console.log("Logger window started"))
-  })
+  }, {once: true});
 });

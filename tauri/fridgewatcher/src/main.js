ea4f124ebcfa83889d70a8ee9eea78a43f74a574
Matthieu Colin <matthieu.colin@etu.minesparis.psl.eu>
2023-07-07 03:22:49 +0200

    Tauri window start

--- tauri/fridgewatcher/src/main.js
+++ tauri/fridgewatcher/src/main.js
@@ -1,4 +1,8 @@
 const { invoke } = window.__TAURI__.tauri;
+const Command = window.__TAURI__.shell.Command
+
+const command = Command.sidecar('logger/logger')
+const output = await command.execute()
 
 let greetInputEl;
 let greetMsgEl;
@@ -20,6 +24,9 @@ window.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#start-logger-button").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#8000ff}</style><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
     })
     console.log("Logger started");
-
+  });
+  document.querySelector('#start-logger-window-button').addEventListener("click", (e) => {
+    console.log("Logger window loop")
+    command.execute().then(() => console.log("Logger window started"))
   })
 });
